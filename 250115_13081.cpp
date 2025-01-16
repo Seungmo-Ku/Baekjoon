177596f82f012a788b6852bb2422ef7297d89dd9
Seungmo Ku <88317380+Seungmo-Ku@users.noreply.github.com>
2025-01-16 15:02:33 +0900

    Solved 13081
    solved 13081

--- 250115_13081.cpp
+++ 250115_13081.cpp
@@ -9,18 +9,25 @@ string getNextRat(string);
 int main(void) {
     int t;
     cin >> t;
+    string* ratsArr = new string[t];
+    int* numArr = new int[t];
+    for(int q = 0; q < t; q ++) {
+        cin >> numArr[q] >> ratsArr[q];
+    }
     for(int q = 0; q < t; q ++) {
     string RATS;
     int rats_to_int;
     int M;
-    cin >> M >> rats_to_int;
-    RATS = to_string(rats_to_int);
+    //cin >> M >> RATS;
+    M = numArr[q];
+    RATS = ratsArr[q];
+    //RATS = to_string(rats_to_int);
     string* ratsString = new string[M];
     //cout << 1 << " :::: " << RATS << endl;
     ratsString[0] = RATS;
     int i;
-    for(i = 1; i < M; i ++) {
-        RATS = getNextRat(RATS);
+    for(i = 0; i < M; i ++) {
+        if(i != 0) RATS = getNextRat(RATS);
         bool exists = false;
         for (int j = 0; j < i; j++) {
             if (ratsString[j] == RATS) {
@@ -68,13 +75,16 @@ int main(void) {
     if(i == M) {
         cout << ratsString[M - 1] << endl;
     }
-    /*
-    for(int i = 0; i < M; i ++) {
-        cout << i+1 << " :::: " << ratsString[i] << endl;
-    }*/
+    
+    //for(int i = 0; i < M; i ++) {
+     //   cout << i+1 << " :::: " << ratsString[i] << endl;
+    //}
     
     delete [] ratsString;
+
     }
+    delete [] ratsArr;
+    delete [] numArr;
     return 0;
 }
 
@@ -83,32 +93,52 @@ string getNextRat(string rat) {
     string nextRat = "";
     
     int length;
-
+    //cout << "rat : " << rat << endl;
     length = rat.length();
+    //cout << "length : " << length << endl;
     
-    long long int* ratArr = new long long int[length];
+    int* ratArr = new int[length];
     for(int i = 0; i < length; i ++) {
+        //cout << "rat i, " << i << " ///// " << rat[i] << " " << rat[length - i - 1] << endl;
         ratArr[i] = rat[i] - '0';
         ratArr[i] += rat[length - i - 1] - '0';
-    }
-    long long int sum = 0;
-    for(int i = 0; i < length; i ++) {
-        int j = length - i - 1;
-        int dup = 1;
-        while(j > 0) {
-            dup *= 10;
-            j --;
+        
+        if(ratArr[i] >= 10) {
+            if(i != 0) {
+                ratArr[i] -= 10;
+                ratArr[i - 1] += 1;
+            }
+                
         }
-        sum += ratArr[i] * dup;
     }
+    for(int i = length - 1; i >= 0; i --) {
+        if(ratArr[i] >= 10) {
+            
+            if(i != 0) {
+                ratArr[i - 1] += 1;
+                ratArr[i] -= 10;
+            }
+        }
+    }
+    string tempRat = "", tempRat2 = "";
+    for(int i = 0; i < length; i ++) {
+        tempRat2 += to_string(ratArr[i]);
+    }
+    //cout << "tempRat2 : " << tempRat2 << endl;
+    for(int i = 0; i < tempRat2.length(); i ++) {
+        if(tempRat2[i] != '0') {
+            tempRat += tempRat2[i];
+        }
+    }
+    //cout << "tempRat : " << tempRat << endl;
+    //tempRat = to_string(sum);
+    //cout << "sum made : " << sum << endl;
     delete [] ratArr;
-    string tempRat;
-    tempRat = to_string(sum);
-    ratArr = new long long int[tempRat.length()];
-    length = 0;
+    ratArr = new int[tempRat.length()];
+    length = tempRat.length();
     for(int i = 0; i < tempRat.length(); i ++) {
         if(tempRat[i] != '0') {
-            ratArr[length++] = tempRat[i] - '0';
+            ratArr[i] = tempRat[i] - '0';
         }
     }
     
